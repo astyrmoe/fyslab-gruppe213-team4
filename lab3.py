062384f806a3188796c88055b7a9968f75ea603a
Aleksander Styrmoe <aleksander@styrmoe.no>
2020-02-25 17:34:54 +0100

    lab3.py updated to figure out the speeds

--- lab3.py
+++ lab3.py
@@ -1,21 +1,42 @@
 import numpy as np
+import sys
 
-file_object  = open("sluttfarter.txt", "r")
+# 'ball' eller 'ring' skal gis som argument!!!
+# argument nummer to er ant forsøk gjort
+
+program=sys.argv[1]
+observations=int(sys.argv[2])
+
+# Samle data
 
 data=[]
 
-for line in file_object:
-    data.append(int(line))
+for observation in range(observations):
+    filename="målinger/"+program+"/"+str(observation+1)+".csv"
+    file_object=open(filename,"r")
+    lines=file_object.readlines()
+    siste=lines[-1].strip().split(",")
+    print(siste)
+    nestsiste=lines[-2].strip().split(",")
+    print(nestsiste)
+    speedx=(float(siste[1])-float(nestsiste[1]))/(float(siste[0])-float(nestsiste[0]))
+    speedy=(float(siste[2])-float(nestsiste[2]))/(float(siste[0])-float(nestsiste[0]))
+    speed=np.sqrt(speedx**2+speedy**2)
+    data.append(speed)
 
-gjennomsnitt=sum(data)/len(data)
+# Regne data
+
+gjennomsnitt=sum(data)/observations
 
 sumsum=0
-for i in range(len(data)):
+for i in range(observations):
     sumsum+=(i-gjennomsnitt)**2
 
-standardavvik=np.sqrt(1/(len(data)-1)*sumsum)
+standardavvik=np.sqrt(1/(observations-1)*sumsum)
 
-standardfeil=standardavvik/np.sqrt(len(data))
+standardfeil=standardavvik/np.sqrt(observations)
+
+# Skrive data
 
 print("Gjennomsnitt: "+str(gjennomsnitt))
 print("Standardavvik: "+str(standardavvik))
