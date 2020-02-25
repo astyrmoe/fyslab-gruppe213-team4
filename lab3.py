7c267b0f3c9e384fccf4991d5701e0fcc3bb352c
Kristian <kristianint@outlook.com>
2020-02-25 18:28:42 +0100

    oppdater lab3.py

--- lab3.py
+++ lab3.py
@@ -4,40 +4,38 @@ import sys
 # 'ball' eller 'ring' skal gis som argument!!!
 # argument nummer to er ant forsøk gjort
 
-program=sys.argv[1]
-observations=int(sys.argv[2])
+program = sys.argv[1]
+observations = int(sys.argv[2])
 
 # Samle data
 
-data=[]
+data = []
 
 for observation in range(observations):
-    filename="målinger/"+program+"/"+str(observation+1)+".csv"
-    file_object=open(filename,"r")
-    lines=file_object.readlines()
-    siste=lines[-1].strip().split(",")
-    print(siste)
-    nestsiste=lines[-2].strip().split(",")
-    print(nestsiste)
-    speedx=(float(siste[1])-float(nestsiste[1]))/(float(siste[0])-float(nestsiste[0]))
-    speedy=(float(siste[2])-float(nestsiste[2]))/(float(siste[0])-float(nestsiste[0]))
-    speed=np.sqrt(speedx**2+speedy**2)
+    filename = "målinger/" + program + "/" + str(observation + 1) + ".csv"
+    file_object = open(filename, "r")
+    lines = file_object.readlines()
+    siste = list(map(float, lines[-1].strip().split(",")))
+    nestsiste = list(map(float, lines[-2].strip().split(",")))
+    speedx = (siste[1] - nestsiste[1]) / (siste[0] - nestsiste[0])
+    speedy = (siste[2] - nestsiste[2]) / (siste[0] - nestsiste[0])
+    speed = np.sqrt(speedx**2 + speedy**2)
     data.append(speed)
 
 # Regne data
 
-gjennomsnitt=sum(data)/observations
+gjennomsnitt = sum(data) / observations
 
-sumsum=0
+SEE = 0
 for i in range(observations):
-    sumsum+=(i-gjennomsnitt)**2
+    SEE += (i - gjennomsnitt)**2
 
-standardavvik=np.sqrt(1/(observations-1)*sumsum)
+standardavvik = np.sqrt(1 / (observations - 1) * SEE)
 
-standardfeil=standardavvik/np.sqrt(observations)
+standardfeil = standardavvik / np.sqrt(observations)
 
 # Skrive data
 
-print("Gjennomsnitt: "+str(gjennomsnitt))
-print("Standardavvik: "+str(standardavvik))
-print("Standardfeil: "+str(standardfeil))
+print("Gjennomsnitt:", gjennomsnitt)
+print("Standardavvik:", standardavvik)
+print("Standardfeil:", standardfeil)
