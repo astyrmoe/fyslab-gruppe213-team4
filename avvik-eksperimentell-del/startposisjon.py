131c99682a38a1762db1ea85325da4ed026097ba
Aleksander Styrmoe <aleksander@styrmoe.no>
2020-03-16 13:54:18 +0100

    legge til scripts som regner ut litt avvik

---
+++ avvik-eksperimentell-del/startposisjon.py
@@ -0,0 +1,37 @@
+import numpy as np
+import sys
+
+# 'ball' eller 'ring' skal gis som argument!!!
+# argument nummer to er ant forsøk gjort
+
+program = sys.argv[1]
+observations = int(sys.argv[2])
+
+# Samle data
+
+data = []
+
+for observation in range(observations):
+    filename = "målinger/" + program + "/" + str(observation + 1) + ".csv"
+    file_object = open(filename, "r")
+    lines = file_object.readlines()
+    data.append(lines[0].strip().split(",")[1])
+
+data = list(map(float, data))
+
+# Regne data
+
+gjennomsnitt = sum(data)/observations
+
+SEE=0
+for i in range(observations):
+    SEE+=(i-gjennomsnitt)**2
+
+standardavvik = np.sqrt(1/(observations-1)*SEE)
+standardfeil = standardavvik / np.sqrt(observations)
+
+# Skrive data
+
+print("Gjennomsnitt:", gjennomsnitt)
+print("Standardavvik:", standardavvik)
+print("Standardfeil:", standardfeil)
